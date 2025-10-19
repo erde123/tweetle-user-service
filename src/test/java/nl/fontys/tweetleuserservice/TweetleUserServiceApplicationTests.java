afc9aa834a408f156e03210f8ea139cd270218f0
erde123 <108157433+erde123@users.noreply.github.com>
2025-10-19 08:45:16 +0200

    ...

--- src/test/java/nl/fontys/tweetleuserservice/TweetleUserServiceApplicationTests.java
+++ src/test/java/nl/fontys/tweetleuserservice/TweetleUserServiceApplicationTests.java
@@ -2,8 +2,10 @@ package nl.fontys.tweetleuserservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles("test")
 class TweetleUserServiceApplicationTests {
 
     @Test
