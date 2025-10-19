298753ea2af976f3fb1ebc7de81a25f720d2b1ea
erde123 <108157433+erde123@users.noreply.github.com>
2025-10-19 08:21:45 +0200

    Add User Repository

---
+++ src/main/java/nl/fontys/tweetleuserservice/persistence/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package nl.fontys.tweetleuserservice.persistence.repository;
+
+import nl.fontys.tweetleuserservice.persistence.entity.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    Optional<UserEntity> findByAuth0Id(String auth0Id);
+}
