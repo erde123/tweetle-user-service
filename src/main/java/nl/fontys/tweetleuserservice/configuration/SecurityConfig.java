04f610301e66dcca047f044465952511670df271
erde123 <108157433+erde123@users.noreply.github.com>
2025-11-05 06:52:00 +0100

    fix swagger

--- src/main/java/nl/fontys/tweetleuserservice/configuration/SecurityConfig.java
+++ src/main/java/nl/fontys/tweetleuserservice/configuration/SecurityConfig.java
@@ -21,8 +21,7 @@ public class SecurityConfig {
                                 "/v3/api-docs/**",
                                 "/swagger-ui/**",
                                 "/swagger-ui.html"
-                        )
-                        .permitAll()
+                        ).permitAll()
                         .anyRequest()
                         .authenticated())
                 .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
